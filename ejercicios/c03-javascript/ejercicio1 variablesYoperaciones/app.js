ed068d99197294d69ec40c7c20f60a530afd3185
adrian <adriangarcia202099@gmail.com>
2026-04-14 22:03:39 -0300

    Ejercicio 1 - Variables y operaciones

---
+++ ejercicios/c03-javascript/ejercicio1 variablesYoperaciones/app.js
@@ -0,0 +1,18 @@
+//Declarar con const: tu nombre, tu edad, la materia que estás cursando
+
+const nombre = "Adrian";
+const edad = 26;
+const materia = "Desarrollo de Software";
+
+//Mostrar en consola un mensaje con template literals
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`)
+
+//Declarar con let un contador en 0. Sumarle 1 tres veces.
+let contador = 0;
+
+contador= contador +1;
+contador= contador +1;
+contador= contador +1;
+
+//Mostrar el valor final.
+console.log(`el valor del contador es: ${contador}`)
